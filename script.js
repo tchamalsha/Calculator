839962d2964ab210472b3cd300458f3a06b5acee
Chamalsha Ubayarathna <62301204+tchamalsha@users.noreply.github.com>
2020-04-01 16:59:36 +0530

    Add files via upload
    This is my first web page.

---
+++ script.js
@@ -0,0 +1,48 @@
+function fn1(){
+    var select=document.getElementById("select1");
+    var a=select.options[select.selectedIndex].value;
+    var inp=document.getElementById("txt1").value;
+     switch(a)
+     {
+             case 1:
+             var ans=inp*187.82;
+             document.write("Rs"+ans);
+             break;
+             case 2:
+            var ans=inp*114.14;
+            document.write("Rs"+ans);
+            break;
+            case 3:
+            var ans=inp*2.46;
+            document.write("Rs"+ans);
+            break;
+             case 4:
+             var ans=inp*600.05;
+             document.write("Rs"+ans);
+             break;
+             case 5:
+             var ans=inp*232.63;
+             document.write("Rs"+ans);
+             break;
+             case 6:
+             var ans=inp*1.75;
+             document.write("Rs"+ans);
+             break;
+             case 7:
+                var ans=inp*2.38;
+                document.write("Rs"+ans);
+                break;
+                case 8:
+                    var ans=inp*26.46;
+                    document.write("Rs"+ans);
+                    break;
+
+                    case 9:
+                        var ans=inp*0.15;
+                        document.write("Rs"+ans);
+                        break;
+        
+     }
+    
+
+}
