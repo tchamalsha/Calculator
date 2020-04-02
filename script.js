2797fe48ff62506869b319b67a21de4280968612
tchamalsha <62301204+tchamalsha@users.noreply.github.com>
2020-04-02 10:15:23 +0530

    Fixed the fuction

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function fn1(){
+    
     var select=document.getElementById("select1");
     var a=select.options[select.selectedIndex].value;
     var inp=document.getElementById("txt1").value;
-     switch(a)
+    
+    
+    
+     /*switch(a)
      {
              case 1:
              var ans=inp*187.82;
-             document.write("Rs"+ans);
-             break;
+            break;
              case 2:
             var ans=inp*114.14;
             document.write("Rs"+ans);
@@ -43,6 +46,36 @@ function fn1(){
                         break;
         
      }
-    
+     document.getElementById("answer").innerHTML="Rs. "+ans;}*/
+    if (a=1){
+     var ans=inp*187.82;
+     document.getElementById("answer").innerHTML="Rs. "+ans;
+    }
+     else if (a=2){
+     var ans=inp*114.14;
+     document.getElementById("answer").innerHTML="Rs. "+ans;}
+     else if (a=3){
+        var ans=inp*2.46;
+        document.getElementById("answer").innerHTML="Rs. "+ans;}
+        else if (a=4){
+            var ans=inp*600.05;
+            document.getElementById("answer").innerHTML="Rs. "+ans;}
+            else if (a=5){
+                var ans=inp*232.63;
+                document.getElementById("answer").innerHTML="Rs. "+ans;}
+                else if (a=6){
+                    var ans=inp*1.75;
+                    document.getElementById("answer").innerHTML="Rs. "+ans;}
+                    else if (a=7){
+                        var ans=inp*2.38;
+                        document.getElementById("answer").innerHTML="Rs. "+ans;}
+                        else if (a=8){
+                            var ans=inp*26.46;
+                            document.getElementById("answer").innerHTML="Rs. "+ans;}
+                            else if (a=9){
+                                var ans=inp*0.15;
+                                document.getElementById("answer").innerHTML="Rs. "+ans;}
+                                                                                                                                        
+     
 
 }
